d8bfd42a6ca8c31939b6003e8556b412d0873460
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2024-02-28 08:48:06 +0100

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './styles/styles.scss'
-import './App.css'
 import StartPage from './pages/Start'
 
 function App() {
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<StartPage />} />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<StartPage />} />
+      </Routes>
     </BrowserRouter>
   )
 }
