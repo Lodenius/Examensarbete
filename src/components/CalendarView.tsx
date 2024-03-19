2b061d2ce280fb4984c1688cf8aae864a65d936e
Lodenius <yhfe22.lodeph@folkuniversitetet.nu>
2024-03-19 12:11:30 +0100

    calendar fix

--- src/components/CalendarView.tsx
+++ src/components/CalendarView.tsx
@@ -20,28 +20,29 @@ const CalendarView: React.FC<CalendarProps> = ({ selectedDate, onSelectDate }) =
     const monthEnd = endOfMonth(currentDate);
     const startDate = startOfWeek(monthStart, { weekStartsOn: 1 });
     const endDate = endOfWeek(monthEnd, { weekStartsOn: 1 });
-
+  
     const days = [];
     let day = startDate;
-
+  
     while (day <= endDate) {
-      const isPastDate = isBefore(day, today);
+      const isTodayDate = isToday(day);
+      const isPastDate = isBefore(day, today) && !isTodayDate;
       const isSelected = isSameDay(day, selectedDate);
       const isDisabled = !isSameMonth(day, monthStart);
       const classNames = ['day'];
-
+  
       if (isSelected) {
         classNames.push('selected');
       }
-
+  
       if (isPastDate) {
         classNames.push('past');
       }
-
+  
       if (isDisabled) {
         classNames.push('disabled');
       }
-
+  
       days.push(
         <div
           key={day.toString()}
@@ -53,16 +54,16 @@ const CalendarView: React.FC<CalendarProps> = ({ selectedDate, onSelectDate }) =
       );
       day = addDays(day, 1);
     }
-
+  
     return days;
   };
 
   return (
     <section className="calendar">
       <section className="calendar__header">
-        <button onClick={() => setCurrentDate(addDays(currentDate, -7))} className='calendar__header--btn'>Previous</button>
+        <button onClick={() => setCurrentDate(addDays(currentDate, -7))} className='calendar__header--btn'> &#60; Previous</button>
         <h2>{format(currentDate, 'MMMM yyyy')}</h2>
-        <button onClick={() => setCurrentDate(addDays(currentDate, 7))} className='calendar__header--btn'>Next</button>
+        <button onClick={() => setCurrentDate(addDays(currentDate, 7))} className='calendar__header--btn'>Next &#62; </button>
       </section>
       <ul className="calendar__days">
         {['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(day => (
